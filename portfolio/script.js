2c2ccae2231bebb810db48324cefe3ce88726d95
Grégory Dusart <gdusart@gmail.com>
2016-11-02 09:05:36 +0100

    Improve scrolling

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,19 +1,60 @@
+function setSelectedMenuItem(anchorId) {
+  var items = $('#navbar > ul > li > a[href^="#"]').each(function() {
+    var link = this;
+    $(link).parent().toggleClass("active", link.hash == anchorId);
+  });
+}
+
 $(document).ready(function(){
     $('a[href^="#"]').on('click',function (e) {
-        var current = getCurrentMenu();
         e.preventDefault();
         var target = this.hash;
         scrollToAnchor(target);
-        current.toggleClass("active");
-        $(this).parent().toggleClass("active");
     });
 });
 
 function scrollToAnchor(divId){
     var container = $(divId);
     $('html,body').animate({scrollTop: container.offset().top - 50},'slow'); /*50px for navbar */
+    setSelectedMenuItem(divId);
 }
 
 function getCurrentMenu() {
   return $("#navbar > ul > li.active");
 }
+
+
+/* Autoscroll on mouse wheel */
+(function() {
+  var delay = false;
+
+  $(document).on('mousewheel DOMMouseScroll', function(event) {
+    event.preventDefault();
+    if(delay) return;
+
+    delay = true;
+    setTimeout(function(){delay = false},200)
+
+    var wd = event.originalEvent.wheelDelta || -event.originalEvent.detail;
+
+    var a = $(".section");
+    if(wd < 0) {
+      for(var i = 0 ; i < a.length ; i++) {
+        var t = a[i].getClientRects()[0].top;
+        if(t >= 60) break;
+      }
+    }
+    else {
+      for(var i = a.length-1 ; i >= 0 ; i--) {
+        var t = a[i].getClientRects()[0].top;
+        if(t < -20) break;
+      }
+    }
+
+    if (typeof(a[i]) === 'undefined') {
+      return;
+    }
+
+    scrollToAnchor("#" + a[i].id);
+  });
+})();
